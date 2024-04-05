02a8dc3778a5f86ea4030516f2902ad1055488a7
Agrima10 <agrimabundela10@gmail.com>
2024-04-05 17:46:11 +0530

    chnages in preprocessing code

---
+++ preprocessing.py
@@ -0,0 +1,173 @@
+import numpy as np
+import cv2
+import os
+import pandas as pd
+import string
+import matplotlib.pyplot as plt
+
+from keras.preprocessing.sequence import pad_sequences
+
+from keras.layers import Dense, LSTM, Reshape, BatchNormalization, Input, Conv2D, MaxPool2D, Lambda, Bidirectional
+from keras.models import Model
+from keras.activations import relu, sigmoid, softmax
+import keras.backend as K
+
+from keras.utils import to_categorical
+from keras.callbacks import ModelCheckpoint
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+from PIL import Image
+
+import tensorflow as tf
+#ignore warnings in the output
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+images = []
+labels = []
+
+RECORDS_COUNT = 25000
+
+train_images = []
+train_labels = []
+train_input_length = []
+train_label_length = []
+train_original_text = []
+
+valid_images = []
+valid_labels = []
+valid_input_length = []
+valid_label_length = []
+valid_original_text = []
+
+inputs_length = []
+labels_length = []
+
+with open('./IAM/words.txt') as f:
+    contents = f.readlines()[18:22539]
+
+lines = [line.strip() for line in contents]
+lines[-1]
+def process_image(img):
+    """
+    Converts image to shape (32, 128, 1) & normalize
+    """
+    w, h = img.shape
+
+#     _, img = cv2.threshold(img,
+#                            128,
+#                            255,
+#                            cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+    # Aspect Ratio Calculation
+    new_w = 32
+    new_h = int(h * (new_w / w))
+    img = cv2.resize(img, (new_h, new_w))
+    w, h = img.shape
+
+    img = img.astype('float32')
+
+    # Converts each to (32, 128, 1)
+    if w < 32:
+        add_zeros = np.full((32-w, h), 255)
+        img = np.concatenate((img, add_zeros))
+        w, h = img.shape
+
+    if h < 128:
+        add_zeros = np.full((w, 128-h), 255)
+        img = np.concatenate((img, add_zeros), axis=1)
+        w, h = img.shape
+
+    if h > 128 or w > 32:
+        dim = (128,32)
+        img = cv2.resize(img, dim)
+
+    img = cv2.subtract(255, img)
+
+    img = np.expand_dims(img, axis=2)
+
+    # Normalize
+    img = img / 255
+
+    return img
+
+max_label_len = 0
+
+char_list = "!\"#&'()*+,-./0123456789:;?ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+# string.ascii_letters + string.digits (Chars & Digits)
+# or
+# "!\"#&'()*+,-./0123456789:;?ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+print(char_list, len(char_list))
+
+def encode_to_labels(txt):
+    # encoding each output word into digits
+    dig_lst = []
+    for index, chara in enumerate(txt):
+        dig_lst.append(char_list.index(chara))
+
+    return dig_lst
+for index, line in enumerate(lines):
+    splits = line.split(' ')
+    status = splits[1]
+
+    if status == 'ok':
+        word_id = splits[0]
+        word = "".join(splits[8:])
+
+        splits_id = word_id.split('-')
+        filepath = './IAM/words/{}/{}-{}/{}.png'.format(splits_id[0],
+                                                  splits_id[0],
+                                                  splits_id[1],
+                                                  word_id)
+
+        # process image
+        img = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+        try:
+            img = process_image(img)
+        except:
+            continue
+
+        # process label
+        try:
+            label = encode_to_labels(word)
+        except:
+            continue
+
+        if index % 10 == 0:
+            valid_images.append(img)
+            valid_labels.append(label)
+            valid_input_length.append(31)
+            valid_label_length.append(len(word))
+            valid_original_text.append(word)
+        else:
+            train_images.append(img)
+            train_labels.append(label)
+            train_input_length.append(31)
+            train_label_length.append(len(word))
+            train_original_text.append(word)
+
+        if len(word) > max_label_len:
+            max_label_len = len(word)
+
+    if index >= RECORDS_COUNT:
+        break
+train_padded_label = pad_sequences(train_labels,
+                             maxlen=max_label_len,
+                             padding='post',
+                             value=len(char_list))
+
+valid_padded_label = pad_sequences(valid_labels,
+                             maxlen=max_label_len,
+                             padding='post',
+                             value=len(char_list))
+
+train_images = np.asarray(train_images)
+train_input_length = np.asarray(train_input_length)
+train_label_length = np.asarray(train_label_length)
+
+valid_images = np.asarray(valid_images)
+valid_input_length = np.asarray(valid_input_length)
+valid_label_length = np.asarray(valid_label_length)
