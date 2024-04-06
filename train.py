11c849ec73e17e2600620b446a6db8d3b0b76d2f
Agrima10 <agrimabundela10@gmail.com>
2024-04-06 15:50:24 +0530

    Update train.py

--- train.py
+++ train.py
@@ -6,6 +6,8 @@ import pandas as pd
 import string
 import matplotlib.pyplot as plt
 from keras.preprocessing.sequence import pad_sequences
+from keras.layers import Dense, LSTM, Reshape, BatchNormalization, Input, Conv2D, MaxPool2D, Lambda, Bidirectional
+from keras.models import Model
 from keras.utils import to_categorical
 from keras.callbacks import ModelCheckpoint
 from sklearn.model_selection import train_test_split
